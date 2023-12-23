bbda767abc3b7eb5c997164b6d7cd0cef478338d
Abimbola <abimbolasobola1@gmail.com>
2023-12-23 20:35:45 +0000

    create project table

--- SalesClasswork.sql
+++ SalesClasswork.sql
@@ -1,3 +1,12 @@
 Drop database if exists SalesClassWork;
 Create DATABASE SalesClassWork;
 Use SalesClassWork;
+
+-- creating table projects
+create table projects (
+id int primary key identity,
+[name] varchar(100),
+[value] decimal(5,2),
+startdate date,
+enddate date
+)

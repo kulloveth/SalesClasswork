95349a902a580c992a26bac2637896a04d4a2b94
chukwuemeka <comerenna@gmail.com>
2023-12-24 00:21:34 +0100

    create an employee table

--- SalesClasswork.sql
+++ SalesClasswork.sql
@@ -1,3 +1,12 @@
 Drop database if exists SalesClassWork;
 Create DATABASE SalesClassWork;
 Use SalesClassWork;
+
+create table employees 
+(
+id int primary key identity,
+firstname varchar(50),
+lastname varchar(50),
+hourlwage decimal(5,2),
+hiredate date
+)
